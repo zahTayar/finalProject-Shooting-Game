bef7b04de9b75aefdc37e65d5d7c0c59f6098d58
zah tayar <zahtayar1993@gmail.com>
2021-05-25 12:58:49 +0300

    Initial commit

---
+++ Assets/Scripts/FPMotion.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class FPMotion : MonoBehaviour
+{
+    public GameObject Acamera;
+    private CharacterController Ccontroller;
+    private float speed = 9f;
+    private float rx = 0f, ry;
+    private float angularSpeed = 2;
+    public GameObject enemy;
+    private AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ccontroller = GetComponent<CharacterController>();
+        audio = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dx = 0, dz = 0;
+        //mouse
+        rx -= Input.GetAxis("Mouse Y") * angularSpeed;
+        //ry += Input.GetAxis("Mouse X") * angularSpeed;
+        ry = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * angularSpeed;
+        Acamera.transform.localEulerAngles = new Vector3(rx, 0, 0);
+
+        transform.localEulerAngles = new Vector3(0, ry, 0);//sets new orientation of player
+
+
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            dx = Input.GetAxis("Horizontal") * speed * Time.deltaTime;//horizontal means "A" "D"        Vector3 motion = new Vector3(dx, 0, dz);
+            dz = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+         
+        }
+
+        //keyboard
+
+
+        Vector3 motion = new Vector3(dx, 0, dz);
+        motion = transform.TransformDirection(motion);
+        Ccontroller.Move(motion);
+
+        //example for primetive motion 
+        /*
+        transform.SetPositionAndRotation(new Vector3(transform.position.x, transform.position.y, transform.position.z + speed),
+            transform.rotation);
+    */
+        //add sound of footsteps
+        if (!audio.isPlaying && this.Ccontroller.velocity.magnitude > 0.1f && (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0))
+        {
+            audio.Play();
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# finalProject
+ Final Project for Game Devlopement course 
